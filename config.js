0898f86ac7f710d6ef0e755a305372112157b78e
irwan-ruskandar <irwanruskandar@gmail.com>
2025-11-21 06:36:47 +0700

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // =============================
 // 🚀 KONFIGURASI API GAS
 // =============================
-const GAS_API = "https://script.google.com/macros/s/AKfycbxxxx/exec";
+const GAS_API = "https://script.google.com/macros/s/AKfycbw3N_YuztERbrhmjOs869Zv7dJcXvZWCNLg9xhN3NuVjm_9Fjqc1SuqJ0MmuUxm6zHoeQ/exec";
 
