4d6936ea150b55727f3d6109ddb49200413592b3
irwan-ruskandar <irwanruskandar@gmail.com>
2025-11-21 06:23:26 +0700

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+/* ========================================================
+   🌐 FRONTEND — Memanggil API GAS
+   ======================================================== */
+
+function loadSDM() {
+  fetch(`${GAS_API}?action=getSDM`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("output").textContent =
+        "SDM DATA:\n\n" + JSON.stringify(data, null, 2);
+    })
+    .catch(err => console.error(err));
+}
+
+function loadLHI() {
+  fetch(`${GAS_API}?action=getLHI`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("output").textContent =
+        "LHI DATA:\n\n" + JSON.stringify(data, null, 2);
+    })
+    .catch(err => console.error(err));
+}
 
